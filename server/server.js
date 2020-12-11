82a544c9bda3dbf818948fa14aaeb196f1072ec4
Joe Buono <joebuono724@gmail.com>
2020-12-10 22:03:20 -0500

    setting up loaderio for proxy

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(PUBLIC_DIR));
 
+app.get('/loaderio-cffe03d6c632a21f548200021daa59a2', function(req, res){
+  res.sendFile(__dirname + '/loaderio-cffe03d6c632a21f548200021daa59a2.txt');
+});
+
 // Handling asset requests for webpack bundles by passing off requests to the bundles router
 app.use('/bundles', router.bundles);
 // Handling AJAX requests to the API by passing off requests to the api router
